5a31fbfda4f4f3ec104428750e1104f3db3630a8
cesaraugusto316 <cesaraugusto316@protonmail.com>
2022-09-14 21:19:47 -0500

    testing with jest and @testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
+/* eslint-disable testing-library/no-debugging-utils */
 import { render, screen } from '@testing-library/react';
+import userEv from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('<App/>', () => {
+
+  it('renderiza el "button" increment', () => {
+    render(<App />);
+
+    const BtnIncement = screen.getByRole('button', { name: /increment/i })
+    const BtnDecrement = screen.getByRole('button', { name: /decrement/i })
+    screen.debug(BtnDecrement)
+
+    expect(BtnDecrement).toBeInTheDocument() // true | false
+    expect(BtnIncement).toBeInTheDocument() // true | false
+  });
+
+  it('should increment by 1 when click button increment', () => {
+    render(<App />);
+
+    const BtnIncement = screen.getByRole('button', { name: /increment/i });
+    const text = screen.getByText('0')
+
+    userEv.click(BtnIncement)
+
+    expect(text).toHaveTextContent('1')
+    screen.debug(text)
+  })
+})
